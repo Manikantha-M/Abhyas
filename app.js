05623c730340c9382756f7a3c8d3520b66017c63
Manikantha-M <manikanta.iiitnuz@gmail.com>
2021-05-01 15:51:25 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// Date
+const date = document.querySelector("#date");
+// ********** nav toggle ************
+// select button and links
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+// add event listener
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
+// set year
+date.innerHTML = new Date().getFullYear();
